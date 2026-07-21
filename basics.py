ac0b45360fe8af67f49adb23562f3d34aa608ca1
Barnak Banerjee <barnakbanerjee6354@gmail.com>
2026-07-21 19:03:43 +0530

    basics2

---
+++ basics.py
@@ -0,0 +1,4 @@
+print ("hello", end=" ")
+print (2)
+print (3+5) #addition
+print ("i am", 21, "years old") #this is comment 
---
+++ variables.py
@@ -0,0 +1,14 @@
+#variables learning
+a = 10
+b = "lol"
+print (a)
+print (b)
+#casting variables
+c1 = int(5)
+print (c1)
+#unpacking
+fruites = ["mango", "lichi", "gooseberry"]
+x, y, z = fruites
+print( x , y , z )
+print( x + y + z )
+print ( c1 + a )
