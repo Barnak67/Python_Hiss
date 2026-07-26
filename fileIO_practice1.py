6123f1cc1fb999bd12f61fb601d8a2c45cb4fc28
Barnak Banerjee <barnakbanerjee6354@gmail.com>
2026-07-26 20:19:28 +0530

    few practice

---
+++ fileIO_practice1.py
@@ -0,0 +1,7 @@
+newfile = open( "txt_demo4.txt", "w" )
+
+newfile.write( "Hello!\nMy name is Barnak"
+              "\nThis is a file created by fileIO operation"
+              "\nI created it using write function " 
+              "\nthis is from fileIO practice1")
+newfile.close()
