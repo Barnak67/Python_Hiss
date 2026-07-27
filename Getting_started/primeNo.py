5116460cdaa7e7c8180f89c8e6c39411be84beab
Barnak Banerjee <barnakbanerjee6354@gmail.com>
2026-07-27 11:31:16 +0530

    few

---
+++ Getting_started/primeNo.py
@@ -0,0 +1,15 @@
+def isPrime(x):
+    count = 0
+    for i in range (1, x+1):
+        if (x % i == 0):
+            count += 1
+    return count == 2
+
+num = int(input("Enter a number: "))
+if isPrime(num):
+    print("PRIME")
+else:
+    print("Not Prime")
+
+
+        
