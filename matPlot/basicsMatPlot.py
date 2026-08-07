91f8b64568b7c907a73fefe1c56c2beab200354d
Barnak Banerjee <barnakbanerjee6354@gmail.com>
2026-08-08 00:48:38 +0530

    matlab basics

---
+++ matPlot/basicsMatPlot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([2020, 2021, 2022, 2023, 2024, 2025])
+y = np.array([500, 470, 430, 520, 600, 750 ])
+
+plt.plot(x, y, marker = ".", markersize = 20, markerfacecolor = "#3de609",
+        markeredgecolor = "000000", linestyle = "dashed")
+plt.show()
